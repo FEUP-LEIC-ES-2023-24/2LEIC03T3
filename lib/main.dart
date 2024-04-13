8303dcba5aa4d8ea10eaa48c10790ffaf760717d
alvaroltorres <alvaroldaatorres@gmail.com>
2024-04-13 20:13:50 +0100

    screens for Sprint 1 created and working: Map and WaterReport

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import 'widgets/home_page.dart'; // Import the HomePage widget
-import 'widgets/water_report.dart'; // Import the WaterReportWidget
-import 'models/water_report_model.dart'; // Import the WaterReportModel
+import 'package:project_es/screens/WaterReport.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,21 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Water Quality App',
+      title: 'DigiWater App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      initialRoute: '/', // Set the initial route
-      routes: {
-        '/': (context) => MultiProvider(
-              providers: [
-                ChangeNotifierProvider(create: (_) => WaterReportModel()),
-              ],
-              child: HomePage(), // Set the HomePage as the initial route
-            ),
-        '/water_report': (context) => WaterReportWidget(), // Route for WaterReportWidget
-      },
+      home: DefaultWaterReportScreen(),
     );
   }
 }
--- lib/screens/Map.dart
+++ lib/screens/Map.dart
@@ -1,111 +1,27 @@
-import '/flutter_flow/flutter_flow_icon_button.dart';
-import '/flutter_flow/flutter_flow_theme.dart';
-import '/flutter_flow/flutter_flow_util.dart';
-import '/flutter_flow/flutter_flow_widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
+//import 'package:google_fonts/google_fonts.dart';
 
-import 'package:project_es/models/map_model.dart';
-export 'package:project_es/models/map_model.dart';
-
-class MapWidget extends StatefulWidget {
-  const MapWidget({super.key});
-
-  @override
-  State<MapWidget> createState() => _MapWidgetState();
-}
-
-class _MapWidgetState extends State<MapWidget> {
-  late MapModel _model;
-
-  final scaffoldKey = GlobalKey<ScaffoldState>();
-
-  @override
-  void initState() {
-    super.initState();
-    _model = createModel(context, () => MapModel());
-  }
-
-  @override
-  void dispose() {
-    _model.dispose();
-
-    super.dispose();
-  }
-
+class MapScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => _model.unfocusNode.canRequestFocus
-          ? FocusScope.of(context).requestFocus(_model.unfocusNode)
-          : FocusScope.of(context).unfocus(),
-      child: Scaffold(
-        key: scaffoldKey,
-        backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
-        appBar: AppBar(
-          backgroundColor: Color(0xFF5BB5DA),
-          automaticallyImplyLeading: false,
-          leading: FlutterFlowIconButton(
-            borderColor: Colors.transparent,
-            borderRadius: 30,
-            borderWidth: 1,
-            buttonSize: 54,
-            icon: FaIcon(
-              FontAwesomeIcons.arrowLeft,
-              color: Colors.white,
-              size: 24,
-            ),
-            onPressed: () async {
-              context.pushNamed('HomePage');
-            },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Map',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+            fontWeight: FontWeight.w600,
           ),
-          title: Align(
-            alignment: AlignmentDirectional(-1, 0),
-            child: Text(
-              'Map',
-              style: FlutterFlowTheme.of(context).titleMedium.override(
-                    fontFamily: 'Poppins',
-                    fontSize: 22,
-                    fontWeight: FontWeight.w600,
-                  ),
-            ),
-          ),
-          actions: [],
-          centerTitle: false,
-          elevation: 0,
         ),
-        body: SafeArea(
-          top: true,
-          child: Stack(
-            children: [
-              Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Expanded(
-                    child: Container(
-                      width: 100,
-                      height: 100,
-                      decoration: BoxDecoration(
-                        color: FlutterFlowTheme.of(context).secondaryBackground,
-                      ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: Image.asset(
-                          'assets/images/MapwPins.png',
-                          width: 300,
-                          height: 200,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
+        backgroundColor: Color(0xFF5bb5da),
+      ),
+      body: Center(
+        child: Text(
+          'Map Screen',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 24,
           ),
         ),
       ),
